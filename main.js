a9b4c6ee9367e28163c1b4ece27d28367b6801d7
Riddhiman2008 <87574062+Riddhiman2008@users.noreply.github.com>
2022-03-18 14:02:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+Webcam.set({
+
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+
+});
+
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot() {
+
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="' + data_uri + '"/>';
+    });
+
+}
+
+console.log('ml5 version:', ml5.version);
+classifier = ml5.imageClasifier('https://teachablemachine.withgoogle.com/models/ldQaU8fVi/teachablemachine.withgoogle.com/models/ldQaU8fVi/model.json', modelLoaded);
+
+function onclick() {
+
+    document.getElementById("result").innerHTML = '<img id="selfie_image" src="' + data_uri + '"/>';
+
+    camera = document.getElementById("camera");
+
+}
